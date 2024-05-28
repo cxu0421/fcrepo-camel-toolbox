2c43f7ef432986477bdfb8232c04ef920a30d41d
Calvin Xu <calvinxu07@gmail.com>
2024-05-28 14:49:35 -0400

    added logging store to file

--- fcrepo-camel-toolbox-app/src/main/java/org/fcrepo/camel/toolbox/app/Driver.java
+++ fcrepo-camel-toolbox-app/src/main/java/org/fcrepo/camel/toolbox/app/Driver.java
@@ -1,12 +1,7 @@
-/*
- * The contents of this file are subject to the license and copyright
- * detailed in the LICENSE and NOTICE files at the root of the source
- * tree.
- */
-
 package org.fcrepo.camel.toolbox.app;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import picocli.CommandLine;
 
@@ -14,22 +9,13 @@ import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
 import static org.fcrepo.camel.common.config.BasePropsConfig.FCREPO_CAMEL_CONFIG_FILE_PROPERTY;
-import static org.slf4j.LoggerFactory.getLogger;
 
-//TODO pull in version and git revision from generated property file
-
-/**
- * The command line tool entry point and parameter definitions
- *
- * @author dbernstein
- */
 @CommandLine.Command(name = "fcrepo-camel-toolbox",
         mixinStandardHelpOptions = true, sortOptions = false,
         versionProvider = AppVersionProvider.class)
 public class Driver implements Callable<Integer> {
 
-    private static final Logger LOGGER = getLogger(Driver.class);
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(Driver.class);
 
     @CommandLine.Option(names = {"--config", "-c"}, required = false, order = 1,
             description = "The path to the configuration file")
@@ -37,11 +23,10 @@ public class Driver implements Callable<Integer> {
 
     @Override
     public Integer call() {
-
         if (configurationFilePath != null) {
             System.setProperty(FCREPO_CAMEL_CONFIG_FILE_PROPERTY, configurationFilePath.toFile().getAbsolutePath());
         }
-        final var appContext = new AnnotationConfigApplicationContext("org.fcrepo.camel");
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext("org.fcrepo.camel");
         appContext.registerShutdownHook();
         appContext.start();
         LOGGER.info("fcrepo-camel-toolbox started.");
@@ -52,14 +37,12 @@ public class Driver implements Callable<Integer> {
         return 0;
     }
 
-    /**
-     * @param args Command line arguments
-     */
     public static void main(final String[] args) {
-        final Driver driver = new Driver();
-        final CommandLine cmd = new CommandLine(driver);
+        Driver driver = new Driver();
+        CommandLine cmd = new CommandLine(driver);
         cmd.setExecutionExceptionHandler(new AppExceptionHandler(driver));
-        cmd.execute(args);
+        int exitCode = cmd.execute(args);
+        System.exit(exitCode);
     }
 
     private static class AppExceptionHandler implements CommandLine.IExecutionExceptionHandler {
@@ -75,11 +58,11 @@ public class Driver implements Callable<Integer> {
                 final Exception ex,
                 final CommandLine commandLine,
                 final CommandLine.ParseResult parseResult) {
+            LOGGER.error("Error during command execution: ", ex);
             commandLine.getErr().println(ex.getMessage());
             ex.printStackTrace(commandLine.getErr());
             commandLine.usage(commandLine.getErr());
             return commandLine.getCommandSpec().exitCodeOnExecutionException();
         }
     }
-
 }
